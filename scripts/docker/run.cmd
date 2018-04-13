49e5f855e4b3790290b366094b345229c1a3facb
Erich Wang <eriwan@microsoft.com>
2018-04-13 18:21:37 +0800

    update script

--- scripts/docker/run.cmd
+++ scripts/docker/run.cmd
@@ -1,7 +1,7 @@
 @ECHO off & setlocal enableextensions enabledelayedexpansion
 
 :: Note: use lowercase names for the Docker images
-SET DOCKER_IMAGE="azureiotpcs/device-simulation-dotnet"
+SET DOCKER_IMAGE="vschinaiot/pcs-device-simulation"
 
 :: strlen("\scripts\docker\") => 16
 SET APP_HOME=%~dp0
